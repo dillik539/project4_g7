fb724306a879f1b91027ccfc40a0ecc85758430d
Airmagic <babaphat@yahoo.com>
2018-03-23 12:45:59 -0500

    added test_article with todos no actual test yet

---
+++ tests/test_article.py
@@ -0,0 +1,13 @@
+# This is a python include file
+from unittest import TestCase
+
+# main testing class to be used by python
+class TestArticle(unittest.TestCase):
+
+    def test_(self):
+        # TODO: make article objects
+
+        # TODO: test returning information
+
+        # TODO comment out when test is writen
+        pass
