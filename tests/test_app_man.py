b42ac1afa4568a10a812df1341c33e323e156373
Airmagic <babaphat@yahoo.com>
2018-03-23 10:45:40 -0500

    added to test_app_man, no actual test yet

--- tests/test_app_man.py
+++ tests/test_app_man.py
@@ -1,5 +1,6 @@
+# This is a python include file
 from unittest import TestCase
-
+# This is the application mangager.py in project4
 from application_manager import menu_controller,get_headlines,get_all,execute_query, query_all_from_date_range,quit_program
 
 # main testing class to be used by python
@@ -11,4 +12,27 @@ class TestAppMan(unittest.TestCase):
 
         # TODO: input something to check the if not in dictionary
 
-    
+        # TODO comment out when test is writen
+        pass
+
+    def test_get_headlines(self):
+        # TODO: don't think we really need to test this one?
+        # TODO comment out when test is writen
+        pass
+
+    def test_get_all(self):
+        # TODO: same thing as get headlines, don't think we have anything to test?
+        # TODO comment out when test is writen1
+        pass
+
+    def test_execute_query(self):
+        # TODO: inset query argument and query type
+
+        # TODO: test to see that article.__str__() returns stuff that contains query argument
+
+        # TODO: test to ensure keyError
+
+    def test_quit_program(self):
+        #TODO: test to ensure statement Farewell digital world...is showen
+
+        
