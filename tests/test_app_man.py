5516747d7aed5eb74f74bf5ade1dabe83ee0ee3f
Airmagic <babaphat@yahoo.com>
2018-03-23 10:29:35 -0500

    added to test document

--- tests/test_app_man.py
+++ tests/test_app_man.py
@@ -1,1 +1,14 @@
 from unittest import TestCase
+
+from application_manager import menu_controller,get_headlines,get_all,execute_query, query_all_from_date_range,quit_program
+
+# main testing class to be used by python
+class TestAppMan(unittest.TestCase):
+
+    # a test to ensure the menu controller works proper
+    def test_menu_controller(self):
+        # TODO:test to check to see if dictionary selection are coming back correctly
+
+        # TODO: input something to check the if not in dictionary
+
+    
